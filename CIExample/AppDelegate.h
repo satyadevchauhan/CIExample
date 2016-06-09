40f8f8840cbd6da26a155fc1b313e1d17e4fbde1
Satyadev Chauhan <satyadevchauhan@gmail.com>
2016-06-09 12:22:04 +0530

    Initial Commit

---
+++ CIExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CIExample
+//
+//  Created by Satyadev Chauhan on 09/06/16.
+//  Copyright © 2016 Satyadev Chauhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CIExampleTests/CIExampleTests.m
@@ -0,0 +1,39 @@
+//
+//  CIExampleTests.m
+//  CIExampleTests
+//
+//  Created by Satyadev Chauhan on 09/06/16.
+//  Copyright © 2016 Satyadev Chauhan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CIExampleTests : XCTestCase
+
+@end
+
+@implementation CIExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
